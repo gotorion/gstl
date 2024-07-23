fdb906871b7e2be2f9fbea862fb11c274447dd78
Junhui Li <junhui.lee@icloud.com>
2024-07-23 22:24:09 +0800

    refactor: add my own is_same_v

--- src/goto_type_traits.h
+++ src/goto_type_traits.h
@@ -208,5 +208,18 @@ TEMPLATE_NULL struct type_traits<signed char const*> {
   typedef true_type has_trivial_destructor;
   typedef true_type is_pod_type;
 };
+
+template <typename T1, typename T2>
+struct is_same {
+  static constexpr bool value = false;
+};
+
+template <typename T>
+struct is_same<T, T> {
+  static constexpr bool value = true;
+};
+
+template <typename T1, typename T2>
+constexpr bool is_same_v = is_same<T1, T2>::value;
 };  // namespace gotostl
 #endif
